a6ec0e2e8967034512f4138780b18cad54662a1c
Austin <austinbv@gmail.com>
2013-02-24 14:39:22 -0700

    a little better setup

--- js/lib/main.js
+++ js/lib/main.js
@@ -1,5 +1,4 @@
 require.config({
-  baseUrl: '.',
   shim: {
     'backbone': {
       deps: ['underscore', 'jquery'],
@@ -9,10 +8,10 @@ require.config({
       exports: '_'
     },
     paths: {
-      'jquery': 'jquery-1.9.1',
-      'backbone': 'backbone-0.9.10',
-      '_': 'underscore-1.4.4',
-      'bootstrap': 'bootstrap-2.3.0'
+      'jquery': './jquery-1.9.1',
+      'backbone': './backbone-0.9.10',
+      '_': './underscore-1.4.4',
+      'bootstrap': './bootstrap-2.3.0'
     }
   }
 });
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ require.config({
   packages: ['lib', 'users', 'repos']
 });
 
-require(['lib/backbone', 'backbone', 'router', 'users', 'repos'], function(Backbone, Router, users, repos) {
+require(['lib', 'lib/backbone', 'router', 'users', 'repos'], function(lib, Backbone, Router, users, repos) {
   var router = new Router();
   router.on("route:users", function() { users.start(); });
   router.on("route:repos", function() { repos.start(); });
